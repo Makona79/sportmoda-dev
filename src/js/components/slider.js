5d1d4c9e4ab02f227552f0d807106bbb9d046e74
Evgenii Kalukov <makona79@yandex.ru>
2022-12-26 06:35:48 +0400

    adaptive news & discont

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -7,10 +7,11 @@ Swiper.use([Navigation, Pagination, Autoplay]);
 const mySwiper = new Swiper(".hero__slider-main", {
   loop: true,
   lazy: true,
-  // autoplay: {
-  //   delay: 5000,
-  // },
-  // If we need pagination
+  autoplay: {
+    delay: 5000,
+  },
+
+
   pagination: {
     el: ".main-slider__pagination",
     clickable: true,
@@ -35,11 +36,28 @@ const sliderSale = new Swiper(".sale-slider", {
   },
 });
 const news = new Swiper(".news-slider", {
-  slidesPerView: 3,
-  spaceBetween: 30,
+  //  slidesPerView: 3,
+  //  spaceBetween: 30,
   loop: true,
   lazy: true,
-
+ // Responsive breakpoints
+ breakpoints: {
+  // when window width is >= 320px
+  375: {
+    slidesPerView: 1,
+    spaceBetween: 10
+  },
+  // when window width is >= 768px
+  768: {
+    slidesPerView: 2,
+    spaceBetween: 20
+  },
+  // when window width is >= 1530px
+  1530: {
+    slidesPerView: 3,
+    spaceBetween: 30
+  }
+},
 
   // Navigation arrows
   navigation: {
